acdf3d028971720d1ca85be7d16dec71daf5b01d
atilaoliveirajr <atila.oliveira.jr@gmail.com>
2021-03-14 15:04:24 +0100

    first commit

---
+++ src/main/java/com/devAtilaOliveira/restAPISpringBoot/RestApiFullStackApplication.java
@@ -0,0 +1,13 @@
+package com.devAtilaOliveira.restAPISpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiFullStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiFullStackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devAtilaOliveira/restAPISpringBoot/RestApiFullStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devAtilaOliveira.restAPISpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiFullStackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
